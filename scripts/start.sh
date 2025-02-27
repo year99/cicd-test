6584a7dfac70284c6c7ec4b5d5e9afdc6d46785d
year99 <53466034+year99@users.noreply.github.com>
2025-02-27 18:22:51 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,35 +1,22 @@
 #!/usr/bin/env bash
 
-# start.sh
-# 서버 구동을 위한 스크립트
+REPOSITORY=/home/ubuntu/cicdproject
+cd $REPOSITORY
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+APP_NAME=springboot-intro
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-REPOSITORY=/home/ubuntu/app
-PROJECT_NAME=springboot-intro
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
 
-cp $REPOSITORY/deploy/*.jar $REPOSITORY/
-
-echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-IDLE_PROFILE=$(find_idle_profile)
-
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
